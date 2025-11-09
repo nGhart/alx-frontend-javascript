98922d5221d8d61293ef49a98595153d10e6fa4a
oGhartey <ondghartey@gmail.com>
2025-11-09 19:49:33 +0000

    correction

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -1,17 +1,17 @@
 import { RowID, RowElement } from "./interface";
 
-export function insertRow(row) {
+declare function insertRow(row: RowElement): number {
   console.log("Insert row", row);
   return Math.floor(Math.random() * Math.floor(1000));
-}
+};
 
-export function deleteRow(rowId) {
+declare function deleteRow(rowId: RowID): void {
   console.log("Delete row id", rowId);
   return;
-}
+};
 
-export function updateRow(rowId, row) {
+declare function updateRow(rowId, row) {
   console.log(`Update row ${rowId}`, row);
 
   return rowId;
-}
+};
